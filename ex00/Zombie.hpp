39a3d9be0c8782858aae606e5ac3aa40ef2db43c
rshaheen <rshaheen@student.codam.nl>
2025-05-05 12:43:39 +0200

    initial files created

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: rshaheen <rshaheen@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/28 16:22:21 by rshaheen      #+#    #+#                 */
-/*   Updated: 2025/04/28 16:51:09 by rshaheen      ########   odam.nl         */
+/*   Updated: 2025/05/05 12:42:37 by rshaheen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ class Zombie
 {
 	private:
 	std::string	name;
-	void	announce( void );
+	
 	public:
 		Zombie(/* args */);
 		~Zombie();
+	void	announce( void ); //in public because subject doec not specify
 };
 
 Zombie::Zombie(/* args */)
